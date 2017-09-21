f277e38239a3a9981ef8523b2f57a05feba48cce
h6266427 <624159426@qq.com>
2017-09-21 16:07:25 +0800

    sql

--- ymshop.sql
+++ ymshop.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50505
 File Encoding         : 65001
 
-Date: 2017-09-21 10:01:02
+Date: 2017-09-21 16:06:30
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -26,7 +26,7 @@ CREATE TABLE `ym_cate` (
   `path` varchar(20) NOT NULL,
   `level` tinyint(4) NOT NULL,
   PRIMARY KEY (`cate_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=24 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of ym_cate
@@ -45,11 +45,42 @@ INSERT INTO `ym_cate` VALUES ('11', '根茎类', '1', '1,11', '1');
 INSERT INTO `ym_cate` VALUES ('12', '青菜', '11', '1,11,12', '2');
 
 -- ----------------------------
+-- Table structure for ym_goods
+-- ----------------------------
+DROP TABLE IF EXISTS `ym_goods`;
+CREATE TABLE `ym_goods` (
+  `goods_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `goods_name` varchar(50) NOT NULL,
+  `face_pic` varchar(100) NOT NULL,
+  `sell_price` decimal(10,0) NOT NULL,
+  `market_price` decimal(10,0) NOT NULL,
+  `marketable` tinyint(4) NOT NULL DEFAULT '1' COMMENT '0-下架 1-上架',
+  `store` int(11) NOT NULL COMMENT '商品库存',
+  `freez` int(11) NOT NULL COMMENT '冻结库存，商品显示数量等于库存减冻结库存',
+  `create_time` char(10) NOT NULL,
+  `last_modify_time` char(10) NOT NULL,
+  `last_modify_id` int(11) NOT NULL,
+  `keywords` varchar(50) NOT NULL,
+  `desc` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `cate_id` int(11) NOT NULL,
+  `is_hot` tinyint(4) NOT NULL DEFAULT '0' COMMENT '是否是热销商品，是为1，否为0',
+  `is_new` tinyint(4) NOT NULL DEFAULT '0' COMMENT '是否是上新商品，是为1，否为0',
+  PRIMARY KEY (`goods_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of ym_goods
+-- ----------------------------
+INSERT INTO `ym_goods` VALUES ('1', '夏威夷果', '', '88', '99', '0', '999', '11', '1505662517', '1505662517', '1', '坚果', '好吃', '来自夏威夷的夏威夷果', '7', '0', '0');
+INSERT INTO `ym_goods` VALUES ('2', '火龙果', '', '55', '66', '1', '100', '80', '1505662517', '1505662517', '2', '水果', '降火', '哈哈哈哈', '2', '0', '0');
+
+-- ----------------------------
 -- Table structure for ym_manager
 -- ----------------------------
 DROP TABLE IF EXISTS `ym_manager`;
 CREATE TABLE `ym_manager` (
-  `manager_id` int(11) NOT NULL,
+  `manager_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `manager_name` varchar(20) NOT NULL,
   `password` varchar(50) NOT NULL,
   `create_time` char(10) NOT NULL,
